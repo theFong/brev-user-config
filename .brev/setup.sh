20ff11351359e011bb5335cca326dce61fcb2161
Alec Fong <alecfong1@gmail.com>
2021-07-26 11:24:17 -0700

    using material for icon theme

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -8,5 +8,6 @@ cp .vscode/settings.json /home/brev/.local/share/code-server/User/settings.json
 code-server --install-extension esbenp.prettier-vscode
 code-server --install-extension eamodio.gitlens
 code-server --install-extension coolbear.systemd-unit-file
-code-server --install-extension vscode-icons-team.vscode-icons
+# code-server --install-extension vscode-icons-team.vscode-icons
+code-server --install-extension pkief.material-icon-theme
 # code-server --install-extension vscodevim.vim
