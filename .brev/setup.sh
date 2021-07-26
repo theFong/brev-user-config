725782565ef6ad8651c0fef3a2fbaf3711bf5b0b
Alec Fong <alecfong1@gmail.com>
2021-07-25 23:45:51 -0700

    adding to setup

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -1,5 +1,8 @@
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sudo apt-get install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+echo "zsh" >> ~/.bashrc
 
+mkdir -p /home/brev/.local/share/code-server/User
 cp .vscode/settings.json /home/brev/.local/share/code-server/User/settings.json
 
 code-server --install-extension esbenp.prettier-vscode
