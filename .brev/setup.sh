992c18238125139b56814a2663de8495260c758a
Alec Fong <alecfong1@gmail.com>
2021-08-27 11:56:54 -0700

    adding duckly as a default and adding more to settings

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -8,7 +8,10 @@ cp .vscode/settings.json /home/brev/.local/share/code-server/User/settings.json
 code-server --install-extension esbenp.prettier-vscode
 code-server --install-extension eamodio.gitlens
 code-server --install-extension coolbear.systemd-unit-file
-# code-server --install-extension vscode-icons-team.vscode-icons
 code-server --install-extension pkief.material-icon-theme
 # code-server --install-extension vscodevim.vim
 code-server --install-extension github.vscode-pull-request-github
+
+export SERVICE_URL=https://open-vsx.org/vscode/gallery
+export ITEM_URL=https://open-vsx.org/vscode/item
+code-server --install-extension gitduck.code-streaming
