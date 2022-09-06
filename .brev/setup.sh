270e0df52bf4ff8f1b5fdec018b4dafdd936d184
alecfong1 <alecfong1@gmail.com>
2022-09-06 18:19:34 +0000

    add docker to user dotbrev

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -17,3 +17,24 @@ export ITEM_URL=https://open-vsx.org/vscode/item
 code-server --install-extension gitduck.code-streaming
 
 git config --global pull.ff only
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+### docker ###
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# https://docs.docker.com/engine/install/linux-postinstall/
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+sudo usermod -aG docker $USER
